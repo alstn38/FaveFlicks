5804bb55dccf03fb1bcaec0f44b2b1a4a6f1fe40
Minsoo Kang <alstn38@naver.com>
2025-01-24 22:29:25 +0900

    feat: StringLiterals ProfileNickName text 추가

--- FaveFlicks/FaveFlicks/Resource/StringLiterals.swift
+++ FaveFlicks/FaveFlicks/Resource/StringLiterals.swift
@@ -14,4 +14,10 @@ enum StringLiterals {
         static let subTitle: String = "당신만의 영화 세상,\nFavaFlicks를 시작해보세요."
         static let startButtonTitle: String = "시작하기"
     }
+    
+    enum ProfileNickName {
+        static let title: String = "프로필 설정"
+        static let nickNamePlaceholder: String = "닉네임을 입력해주세요."
+        static let confirmButtonTitle: String = "완료"
+    }
 }
