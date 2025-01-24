b1f889bea4901163352a16f3ab54f19198431be0
Minsoo Kang <alstn38@naver.com>
2025-01-24 18:42:18 +0900

    feat: StringLiterals Onboarding String 추가

---
+++ FaveFlicks/FaveFlicks/Resource/StringLiterals.swift
@@ -0,0 +1,17 @@
+//
+//  StringLiterals.swift
+//  FaveFlicks
+//
+//  Created by 강민수 on 1/24/25.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum Onboarding {
+        static let title: String = "Onboarding"
+        static let subTitle: String = "당신만의 영화 세상,\nFavaFlicks를 시작해보세요."
+        static let startButtonTitle: String = "시작하기"
+    }
+}
