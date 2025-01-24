0cdd7ef1b0a9ba783b3f0f6ff5e2dae754aee954
Minsoo Kang <alstn38@naver.com>
2025-01-24 17:40:41 +0900

    init: FaveFlicks Project 생성

---
+++ FaveFlicks/FaveFlicks/Present/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaveFlicks
+//
+//  Created by 강민수 on 1/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
