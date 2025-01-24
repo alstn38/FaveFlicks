a9a373d20df465459aada1dc390cf1fe76196caf
Minsoo Kang <alstn38@naver.com>
2025-01-25 00:43:54 +0900

    feat: UICollectionViewCell+Extension identifier 선언

---
+++ FaveFlicks/FaveFlicks/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+.swift
+//  FaveFlicks
+//
+//  Created by 강민수 on 1/25/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
