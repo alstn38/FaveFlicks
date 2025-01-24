bca4dfb498758b37a7fc5de001e0b1001dfdf898
Minsoo Kang <alstn38@naver.com>
2025-01-24 23:46:36 +0900

    chore: StringLiterals NickNameStatus text 추가

--- FaveFlicks/FaveFlicks/Resource/StringLiterals.swift
+++ FaveFlicks/FaveFlicks/Resource/StringLiterals.swift
@@ -23,5 +23,9 @@ enum StringLiterals {
         static let title: String = "프로필 설정"
         static let nickNamePlaceholder: String = "닉네임을 입력해주세요."
         static let confirmButtonTitle: String = "완료"
+        static let possibleStatus: String = "사용할 수 있는 닉네임이에요"
+        static let invalidRangeStatus: String = "2글자 이상 10글자 미만으로 설정해주세요"
+        static let hasSectionSignStatus: String = "낙내암애 @, #, $, %는 포함할 수 없어요"
+        static let hasNumberStatus: String = "닉네임에 숫자는 포함할 수 없어요"
     }
 }
