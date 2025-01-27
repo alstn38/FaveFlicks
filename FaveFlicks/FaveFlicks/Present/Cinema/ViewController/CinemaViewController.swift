9880228ad7e1fed2fbcc81392b1aac5f08352423
Minsoo Kang <alstn38@naver.com>
2025-01-27 20:59:59 +0900

    feat: CinemaViewController 오늘의 영화 Delegate, DataSource 설정

--- FaveFlicks/FaveFlicks/Present/Cinema/ViewController/CinemaViewController.swift
+++ FaveFlicks/FaveFlicks/Present/Cinema/ViewController/CinemaViewController.swift
@@ -43,6 +43,13 @@ final class CinemaViewController: UIViewController {
             RecentSearchedCollectionViewCell.self,
             forCellWithReuseIdentifier: RecentSearchedCollectionViewCell.identifier
         )
+        
+        cinemaView.todayMovieCollectionView.delegate = self
+        cinemaView.todayMovieCollectionView.dataSource = self
+        cinemaView.todayMovieCollectionView.register(
+            TodayMovieCollectionViewCell.self,
+            forCellWithReuseIdentifier: TodayMovieCollectionViewCell.identifier
+        )
     }
     
     @objc private func searchButtonDidTap(_ sender: UIBarButtonItem) {
@@ -50,20 +57,56 @@ final class CinemaViewController: UIViewController {
     }
 }
 
-// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
-extension CinemaViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+extension CinemaViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sampleRecentSearched.count
+        switch collectionView {
+        case cinemaView.recentSearchedCollectionView:
+            return sampleRecentSearched.count
+            
+        case cinemaView.todayMovieCollectionView:
+            return 10
+            
+        default:
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: RecentSearchedCollectionViewCell.identifier,
-            for: indexPath
-        ) as? RecentSearchedCollectionViewCell else { return UICollectionViewCell() }
-        
-        cell.configureCell(sampleRecentSearched[indexPath.item])
-        return cell
+        switch collectionView {
+        case cinemaView.recentSearchedCollectionView:
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: RecentSearchedCollectionViewCell.identifier,
+                for: indexPath
+            ) as? RecentSearchedCollectionViewCell else { return UICollectionViewCell() }
+            
+            cell.configureCell(sampleRecentSearched[indexPath.item])
+            return cell
+            
+        case cinemaView.todayMovieCollectionView:
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: TodayMovieCollectionViewCell.identifier,
+                for: indexPath
+            ) as? TodayMovieCollectionViewCell else { return UICollectionViewCell() }
+            
+            return cell
+            
+        default:
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch collectionView {
+        case cinemaView.todayMovieCollectionView:
+            let height = cinemaView.todayMovieCollectionView.frame.height
+            let width = height * 3 / 5
+            
+            return CGSize(width: width, height: height)
+            
+        default:
+            return .zero
+        }
     }
 }
