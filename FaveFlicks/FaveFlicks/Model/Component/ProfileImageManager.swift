72e08b7c6064223ee7dd0f4ea55074cfe0612d0a
Minsoo Kang <alstn38@naver.com>
2025-01-27 16:13:13 +0900

    feat: ProfileImageManager 정의

---
+++ FaveFlicks/FaveFlicks/Model/Component/ProfileImageManager.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileImageManager.swift
+//  FaveFlicks
+//
+//  Created by 강민수 on 1/27/25.
+//
+
+import UIKit
+
+final class ProfileImageManager {
+    
+    let profileImageArray: [UIImage] = [
+        .profile0, .profile1, .profile2, .profile3, .profile4, .profile5, .profile6, .profile7,.profile8,.profile9,.profile10,.profile11
+    ]
+    
+    func getCurrentProfileImage() -> UIImage {
+        let currentImageIndex = UserDefaultManager.shared.profileImageIndex
+        return profileImageArray[currentImageIndex]
+    }
+    
+    func getProfileImage(at index: Int) -> UIImage? {
+        guard index >= 0 && index < profileImageArray.count else { return nil }
+        return profileImageArray[index]
+    }
+}
