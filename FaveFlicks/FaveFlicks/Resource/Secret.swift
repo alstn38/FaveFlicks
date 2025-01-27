2a41f9fa78a84eadb2dbada3719d2fc2f29a11f8
Minsoo Kang <alstn38@naver.com>
2025-01-27 22:57:00 +0900

    feat: Secret API Key, main URL 설정 및 적용

---
+++ FaveFlicks/FaveFlicks/Resource/Secret.swift
@@ -0,0 +1,24 @@
+//
+//  Secret.swift
+//  FaveFlicks
+//
+//  Created by 강민수 on 1/27/25.
+//
+
+import Foundation
+
+enum Secret {
+    static let baseURL: String = {
+        guard let urlString = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            fatalError("BASE_URL ERROR")
+        }
+        return urlString
+    }()
+    
+    static let accessToken: String = {
+        guard let accessToken = Bundle.main.infoDictionary?["ACCESS_TOKEN"] as? String else {
+            fatalError("ACCESS_TOKEN ERROR")
+        }
+        return accessToken
+    }()
+}
