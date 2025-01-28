58d72cc30dc152ce565afb5ee41ba071d4db679b
Minsoo Kang <alstn38@naver.com>
2025-01-28 22:27:57 +0900

    feat: SearchCollectionViewCell View 설정 및 Layout 설정

---
+++ FaveFlicks/FaveFlicks/Present/Search/Cell/SearchCollectionViewCell.swift
@@ -0,0 +1,127 @@
+//
+//  SearchCollectionViewCell.swift
+//  FaveFlicks
+//
+//  Created by 강민수 on 1/28/25.
+//
+
+import Kingfisher
+import SnapKit
+import UIKit
+
+final class SearchCollectionViewCell: UICollectionViewCell {
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    private let movieTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.textColor = UIColor(resource: .faveFlicksWhite)
+        label.numberOfLines = 2
+        label.text = "엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글"
+        return label
+    }()
+    
+    private let movieDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.textColor = UIColor(resource: .faveFlicksLightGray)
+        label.numberOfLines = 1
+        label.text = "엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글엄청긴글"
+        return label
+    }()
+    
+    private let genreStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 4
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private let favoriteButton: UIButton = {
+        var imageConfiguration = UIImage.SymbolConfiguration(pointSize: 18)
+        var configuration = UIButton.Configuration.plain()
+        configuration.preferredSymbolConfigurationForImage = imageConfiguration
+        configuration.baseForegroundColor = UIColor(resource: .faveFlicsMain)
+        
+        let button = UIButton(configuration: configuration)
+        button.setImage(
+            UIImage(systemName: "heart", withConfiguration: UIImage.SymbolConfiguration(weight: .medium)),
+            for: .normal
+        )
+        button.setImage(
+            UIImage(systemName: "heart.fill", withConfiguration: UIImage.SymbolConfiguration(weight: .medium)),
+            for: .selected
+        )
+        return button
+    }()
+    
+    private let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(resource: .faveFlicksGray)
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureHierarchy() {
+        addSubviews(
+            posterImageView,
+            movieTitleLabel,
+            movieDateLabel,
+            genreStackView,
+            favoriteButton,
+            lineView
+        )
+    }
+    
+    private func configureLayout() {
+        posterImageView.snp.makeConstraints {
+            $0.verticalEdges.leading.equalToSuperview().inset(15)
+            $0.width.equalTo(80)
+        }
+        
+        movieTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(posterImageView).offset(4)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(12)
+            $0.trailing.equalToSuperview().inset(15)
+        }
+        
+        movieDateLabel.snp.makeConstraints {
+            $0.top.equalTo(movieTitleLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(12)
+        }
+        
+        genreStackView.snp.makeConstraints {
+            $0.bottom.equalTo(posterImageView.snp.bottom)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(12)
+        }
+        
+        favoriteButton.snp.makeConstraints {
+            $0.size.equalTo(20)
+            $0.trailing.bottom.equalToSuperview().inset(15)
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(15)
+            $0.height.equalTo(1)
+        }
+    }
+}
