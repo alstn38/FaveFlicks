bb8305a18a87a627e76b5d1634d1b4e2034c3511
Minsoo Kang <alstn38@naver.com>
2025-01-30 22:22:50 +0900

    feat: NotificationName+Extension UpdateUserInfo 추가

---
+++ FaveFlicks/FaveFlicks/Extension/NotificationName+.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName+.swift
+//  FaveFlicks
+//
+//  Created by 강민수 on 1/30/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    
+    static let updateUserInfo = Notification.Name("updateUserInfo")
+}
