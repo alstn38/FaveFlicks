8469784cceee671242d051ed0c3dc2759e1c2dcc
Minsoo Kang <alstn38@naver.com>
2025-01-30 20:32:34 +0900

    feat: SettingViewController 탈퇴 기능 구현

--- FaveFlicks/FaveFlicks/Present/Setting/ViewController/SettingViewController.swift
+++ FaveFlicks/FaveFlicks/Present/Setting/ViewController/SettingViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 final class SettingViewController: UIViewController {
     
     private let settingView = SettingView()
-    private let settingTitleArray: [SettingItem] = SettingItem.allCases
+    private let settingItemArray: [SettingItem] = SettingItem.allCases
     
     override func loadView() {
         view = settingView
@@ -41,7 +41,7 @@ final class SettingViewController: UIViewController {
 extension SettingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return settingTitleArray.count
+        return settingItemArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -50,8 +50,37 @@ extension SettingViewController: UICollectionViewDelegate, UICollectionViewDataS
             for: indexPath
         ) as? SettingCollectionViewCell else { return UICollectionViewCell() }
         
-        cell.configureCell(settingTitleArray[indexPath.item].description)
+        cell.configureCell(settingItemArray[indexPath.item].description)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let settingItem = settingItemArray[indexPath.item]
+        
+        switch settingItem {
+        case SettingItem.frequentlyAskedQuestions:
+            return
+        case SettingItem.oneOnoneInquiry:
+            return
+        case SettingItem.setNotifications:
+            return
+        case SettingItem.deleteAccount:
+            presentAlertWithCancel(title: StringLiterals.Setting.deleteAlertTitle, message: StringLiterals.Setting.deleteAlertMessage) {
+                guard
+                    let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                    let window = windowScene.windows.first
+                else { return }
+                
+                let onboardingNavigationController = UINavigationController(rootViewController: OnboardingViewController())
+                onboardingNavigationController.view.alpha = 0.5
+                UIView.animate(withDuration: 0.4, delay: 0, options: .curveLinear) {
+                    onboardingNavigationController.view.alpha = 1.0
+                }
+                
+                UserDefaultManager.shared.deleteAccount()
+                window.rootViewController = onboardingNavigationController
+            }
+        }
     }
 }
 
