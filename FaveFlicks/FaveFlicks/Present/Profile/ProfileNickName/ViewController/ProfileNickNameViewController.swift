d5d5b6a1c7887db151b940fd609c245be437f91f
Minsoo Kang <alstn38@naver.com>
2025-01-30 21:43:38 +0900

    feat: present Style에 따른 프로필 설정 화면 구성

--- FaveFlicks/FaveFlicks/Present/Profile/ProfileNickName/ViewController/ProfileNickNameViewController.swift
+++ FaveFlicks/FaveFlicks/Present/Profile/ProfileNickName/ViewController/ProfileNickNameViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class ProfileNickNameViewController: UIViewController {
     
+    private let presentationStyleType: PresentationStyleType
     private let profileNickNameView = ProfileNickNameView()
     private var nickNameStatus: ProfileNickNameView.NickNameStatus = .invalidRange
     private let profileImageManager = ProfileImageManager()
@@ -23,7 +24,17 @@ final class ProfileNickNameViewController: UIViewController {
         formatter.dateFormat = StringLiterals.ProfileNickName.joinDateFormatter
         return formatter
     }()
-
+    
+    init(presentationStyleType: PresentationStyleType) {
+        self.presentationStyleType = presentationStyleType
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         view = profileNickNameView
     }
@@ -45,6 +56,25 @@ final class ProfileNickNameViewController: UIViewController {
     private func configureNavigation() {
         navigationItem.title = StringLiterals.ProfileNickName.title
         navigationItem.backButtonTitle = StringLiterals.NavigationItem.backButtonTitle
+        
+        if presentationStyleType == .modal {
+            let cancelButton = UIBarButtonItem(
+                image: UIImage(systemName: "xmark"),
+                style: .plain,
+                target: self,
+                action: #selector(cancelButtonDidTap)
+            )
+            
+            let saveButton = UIBarButtonItem(
+                title: StringLiterals.ProfileNickName.saveButtonTitle,
+                style: .plain,
+                target: self,
+                action: #selector(saveButtonDidTap)
+            )
+            
+            navigationItem.leftBarButtonItem = cancelButton
+            navigationItem.rightBarButtonItem = saveButton
+        }
     }
     
     private func configureProfileImage() {
@@ -63,7 +93,23 @@ final class ProfileNickNameViewController: UIViewController {
     }
     
     private func configureButton() {
-        profileNickNameView.confirmButton.addTarget(self, action: #selector(confirmButtonDidTap), for: .touchUpInside)
+        switch presentationStyleType {
+        case PresentationStyleType.push:
+            profileNickNameView.confirmButton.addTarget(self, action: #selector(confirmButtonDidTap), for: .touchUpInside)
+            
+        case PresentationStyleType.modal:
+            profileNickNameView.confirmButton.isHidden = true
+        }
+    }
+    
+    private func saveUserInfo() {
+        UserDefaultManager.shared.hasProfile = true
+        UserDefaultManager.shared.profileImageIndex = selectedProfileImageIndex
+        UserDefaultManager.shared.nickName = profileNickNameView.nickNameTextField.text ?? ""
+        
+        if presentationStyleType == .push {
+            UserDefaultManager.shared.joinDate = dateFormatter.string(from: Date())
+        }
     }
     
     @objc private func profileImageViewDidTap(_ sender: UIView) {
@@ -104,25 +150,33 @@ final class ProfileNickNameViewController: UIViewController {
             return presentAlert(title: StringLiterals.Alert.invalidNickName, message: nickNameStatus.description)
         }
         
-        UserDefaultManager.shared.hasProfile = true
-        UserDefaultManager.shared.profileImageIndex = selectedProfileImageIndex
-        print("NickName = \(profileNickNameView.nickNameTextField.text ?? "")")
-        print("joinDate = \(dateFormatter.string(from: Date()))")
-        UserDefaultManager.shared.nickName = profileNickNameView.nickNameTextField.text ?? ""
-        UserDefaultManager.shared.joinDate = dateFormatter.string(from: Date())
-        let faveFlicksTabBarController = FaveFlicksTabBarController()
+        saveUserInfo()
         
         guard
             let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
             let window = windowScene.windows.first
         else { return }
         
+        let faveFlicksTabBarController = FaveFlicksTabBarController()
         faveFlicksTabBarController.view.alpha = 0.5
         UIView.animate(withDuration: 0.4, delay: 0, options: .curveLinear) {
             faveFlicksTabBarController.view.alpha = 1.0
         }
         
         window.rootViewController = faveFlicksTabBarController
+    }
+    
+    @objc private func cancelButtonDidTap(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    @objc private func saveButtonDidTap(_ sender: UIBarButtonItem) {
+        guard nickNameStatus == .possible else {
+            return presentAlert(title: StringLiterals.Alert.invalidNickName, message: nickNameStatus.description)
+        }
+        
+        saveUserInfo()
+        dismiss(animated: true)
     }
 }
 
@@ -141,5 +195,14 @@ extension ProfileNickNameViewController: ProfileImageViewControllerDelegate {
     func viewController(_ viewController: UIViewController, didSelectImageIndex: Int) {
         selectedProfileImageIndex = didSelectImageIndex
         configureProfileImage()
+    }
+}
+
+// MARK: - PresentationStyle
+extension ProfileNickNameViewController {
+    
+    enum PresentationStyleType {
+        case push
+        case modal
     }
 }
