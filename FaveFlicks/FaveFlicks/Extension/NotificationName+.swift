9cd81b02c25808e66573cb932afac5a2837c686a
Minsoo Kang <alstn38@naver.com>
2025-01-30 23:25:54 +0900

    feat: Notification+Extension UpdateRecentSearchTextArray 추가

--- FaveFlicks/FaveFlicks/Extension/NotificationName+.swift
+++ FaveFlicks/FaveFlicks/Extension/NotificationName+.swift
@@ -10,4 +10,5 @@ import Foundation
 extension Notification.Name {
     
     static let updateUserInfo = Notification.Name("updateUserInfo")
+    static let updateRecentSearchTextArray = Notification.Name("updateRecentSearchTextArray")
 }
